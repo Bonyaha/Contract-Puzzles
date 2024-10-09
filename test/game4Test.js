6e28f12b05bf190fe14dd0cb540059bb72bcf5c0
Roman <romchikus1987@gmail.com>
2024-10-09 13:05:46 +0300

    won

--- test/game4Test.js
+++ test/game4Test.js
@@ -6,14 +6,23 @@ describe('Game4', function () {
     const Game = await ethers.getContractFactory('Game4');
     const game = await Game.deploy();
 
-    return { game };
+    const [signer, acc1, acc2, acc3] = await ethers.getSigners();
+    const address = await signer.getAddress();
+    const address1 = await acc1.getAddress();
+
+    console.log("Signer Address:", address);
+    console.log("Account1 Address:", address1);
+
+    return { game, address, address1, acc1};
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, address, address1, acc1 } = await loadFixture(deployContractAndSetVariables);
 
+    //console.log('acc1 is: ', acc1);
+    
     // nested mappings are rough :}
-
-    await game.win();
+    await game.write(address1);
+    await game.connect(acc1).win(address);
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
