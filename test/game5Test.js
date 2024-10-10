1d6758a639313a036d706c49d7375984b23e25bd
Roman <romchikus1987@gmail.com>
2024-10-10 07:23:49 +0300

    won

--- test/game5Test.js
+++ test/game5Test.js
@@ -6,14 +6,32 @@ describe('Game5', function () {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
 
-    return { game };
+    //const signers = await ethers.getSigners();  // Get the list of available signers
+
+    // Create a wallet with an address that starts with 0x00
+    let wallet;
+    while (true) {
+      wallet = ethers.Wallet.createRandom().connect(ethers.provider);
+      if (wallet.address.slice(0, 4).toLowerCase() === '0x00') {
+        break;
+      }
+    }
+    
+    // Get some ETH to the wallet to pay for gas
+    const signer = await ethers.provider.getSigner();
+    await signer.sendTransaction({
+      to: wallet.address,
+      value: ethers.parseEther('1'),
+    });
+
+   
+    return { game,wallet };
   }
+
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
-
-    // good luck
-
-    await game.win();
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
+   
+    await game.connect(wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
